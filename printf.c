c68b1da909af4dfc4991ad7f6b3137b4fc89ef3c
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-26 15:51:13 +0100

    maj

--- printf.c
+++ printf.c
@@ -1,63 +1,103 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdint.h>
 
-int _printf(const char *format, ...)
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
 {
-	int count = 0; // To count the number of characters printed
-	va_list args;  // To handle the variable arguments
+	return write(1, &c, 1); /*Write the character to stdout*/
+}
 
-	va_start(args, format); // Initialize the argument list
+/**
+ * spe - processes format specifiers and prints accordingly
+ * @format: the format string
+ * @i: pointer to the current index in the format string
+ * @args: the variable argument list
+ * @printed_chars: pointer to the count of printed characters
+ *
+ * Return: the number of characters printed by this function
+ */
+int spe(const char *format, unsigned int *i, va_list args, int *printed_chars)
+{
+	int count = 0; /*To count characters printed by this function*/
 
-	for (const char *p = format; *p != '\0'; p++)
+	switch (format[*i])
 	{
-		if (*p == '%')
-		{		 // Check for format specifier
-			p++; // Move to the next character
-			switch (*p)
-			{
-			case 'c':
-			{									  // Character
-				char c = (char)va_arg(args, int); // Get the character argument
-				putchar(c);						  // Print the character
-				count++;						  // Increment count
-				break;
-			}
-			case 's':
-			{									// String
-				char *s = va_arg(args, char *); // Get the string argument
-				if (s == NULL)
-				{ // Handle NULL string
-					s = "(null)";
-				}
-				while (*s)
-				{ // Print each character of the string
-					putchar(*s);
-					s++;
-					count++; // Increment count
-				}
-				break;
-			}
-			case '%':		  // Percent sign
-				putchar('%'); // Print percent sign
-				count++;	  // Increment count
-				break;
-			default:		  // Invalid format specifier
-				putchar('%'); // Print the '%' character
-				putchar(*p);  // Print the invalid specifier
-				count += 2;	  // Increment count by 2
-				break;
-			}
+	case 'c':
+	{									  /*Character*/
+		char c = (char)va_arg(args, int); /*Get the character argument*/
+		count += _putchar(c);			  /*Print and count the character*/
+		break;
+	}
+	case 's':
+	{									/*String*/
+		char *s = va_arg(args, char *); /*Get the string argument*/
+		if (s == NULL)
+		{ /*Handle NULL string*/
+			s = "(null)";
 		}
-		else
-		{				 // Regular character
-			putchar(*p); // Print the character
-			count++;	 // Increment count
+		while (*s)
+		{ /*Print each character of the string*/
+			count += _putchar(*s);
+			s++;
 		}
+		break;
+	}
+	case '%':					/*Percent sign*/
+		count += _putchar('%'); /*Print percent sign*/
+		break;
+	default:						   /*Invalid format specifier*/
+		count += _putchar('%');		   /*Print the '%' character*/
+		count += _putchar(format[*i]); /*Print the invalid specifier*/
+		break;
 	}
 
-	va_end(args); // Clean up the argument list
+	*i += 1;	/* Move to the next character after the specifier*/
+	return count; /*Return the count of characters printed*/
+}
 
-	return count; // Return the total count of printed characters
+/**
+ * _printf - a simple version of the printf function
+ * @format: the format string containing the text to be written to stdout
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	int printed_chars = 0;
+
+	if (format == NULL) /* check if format is NULL */
+		return (-1);
+
+	va_start(args, format); /* initialize the argument list */
+
+	while (format[i]) /* iterate through the format string */
+	{
+		if (format[i] == '%')
+		{
+			i++; /* move to the next character */
+			printed_chars += spe(format, &i, args, &printed_chars);
+		}
+		else
+		{
+			/* prints formatted output and counts characters. */
+			printed_chars += _putchar(format[i]);
+		}
+		i++;
+	}
+
+	va_end(args);			/* end using the argument list */
+	return (printed_chars); /* return the number of characters printed */
 }
 
 int main()
