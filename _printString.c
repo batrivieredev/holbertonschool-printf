3d93a2d703cd951ba56b4868d07791f9545c2a5f
batrivieredev <baptiste.rivierefr@gmail.com>
2024-11-26 16:57:10 +0100

    percent,char,string

---
+++ _printString.c
@@ -0,0 +1,41 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _printString - Prints a string to stdout.
+ * @args: The list of arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int _printString(va_list args)
+{
+	char *str;
+	char *copied_str;
+	int count = 0;
+
+
+	str = va_arg(args, char *);
+
+
+	if (str == NULL)
+		str = "(null)";
+
+	/* Copy the string */
+	copied_str = _strcopy(str);
+	if (copied_str == NULL)
+		return 0;
+
+	while (*copied_str)
+	{
+		_putchar(*copied_str);
+		count++;
+		copied_str++;
+	}
+
+	free(copied_str - count);
+
+	return count;
+}
